bb76e8599cb08577d5486d4755e335e7b4705f6b
arnhav <arnhavcam@gmail.com>
2024-04-24 10:33:06 -0500

    Add update method override for ActionMenu

--- src/main/java/com/ags/menuapi/Menu/ActionMenu.java
+++ src/main/java/com/ags/menuapi/Menu/ActionMenu.java
@@ -10,6 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ActionMenu extends Menu {
@@ -66,6 +67,12 @@ public class ActionMenu extends Menu {
     @Override
     public void addItemToAllPages(int slot, MenuItem mItem) {
         super.addItemToAllPages(slot, mItem);
+        actions.put(mItem.getItem(), mItem.getAction());
+    }
+
+    @Override
+    public void updateItemDesc(MenuItem mItem, List<String> lines) {
+        super.updateItemDesc(mItem, lines);
         actions.put(mItem.getItem(), mItem.getAction());
     }
 
